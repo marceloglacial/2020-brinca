230544a21368c0ad855443baad5b14f6b8d9e2ff
Marcelo Glacial <glacial@gmail.com>
2020-08-02 17:01:30 -0400

    initial navbar

--- frontend/components/Header/index.js
+++ frontend/components/Header/index.js
@@ -1,5 +1,6 @@
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import Logo from 'components/Logo';
+import Link from 'next/link';
 
 const Header = (props) => {
   return (
@@ -10,18 +11,21 @@ const Header = (props) => {
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
-          <Nav.Link href='#link'>Link</Nav.Link>
-          <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-            <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.2'>
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href='#action/3.4'>
-              Separated link
-            </NavDropdown.Item>
+          <Link href='/fale-conosco'>
+            <Nav.Link href='/fale-conosco'>Quem Somos</Nav.Link>
+          </Link>
+          <Link href='/eventos/'>
+            <Nav.Link href='/eventos/'>Eventos</Nav.Link>
+          </Link>
+          <Link href='/fale-conosco'>
+            <Nav.Link href='/fale-conosco'>Fale Conosco</Nav.Link>
+          </Link>
+        </Nav>
+        <Nav>
+          <NavDropdown title='Language' id='basic-nav-dropdown'>
+            <NavDropdown.Item>Português</NavDropdown.Item>
+            <NavDropdown.Item>English</NavDropdown.Item>
+            <NavDropdown.Item>Frensh</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
