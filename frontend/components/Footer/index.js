16d5ddd5593dd72d383fd54d02e623694916e005
Marcelo Glacial <glacial@gmail.com>
2020-08-02 18:47:47 -0400

    add copy

--- frontend/components/Footer/index.js
+++ frontend/components/Footer/index.js
@@ -4,24 +4,28 @@ import styles from './styles.module.scss';
 
 const Footer = (props) => {
   return (
-    <Container className={styles.footer} fluid>
-      <Nav className='justify-content-center py-3'>
-        <Link href='/fale-conosco'>
-          <Nav.Link href='/fale-conosco'>Associe-se</Nav.Link>
-        </Link>
-        <Link href='/eventos/'>
-          <Nav.Link href='/eventos/'>FaceBook</Nav.Link>
-        </Link>
-        <Link href='/fale-conosco'>
-          <Nav.Link href='/fale-conosco'>Quem Somos</Nav.Link>
-        </Link>
-      </Nav>
-      <Nav className='justify-content-center pb-3'>
-        <Link href='/fale-conosco'>
-          <Nav.Item>Copyrights Brinca - 2020</Nav.Item>
-        </Link>
-      </Nav>
-    </Container>
+    <>
+      <Container className={styles.footer} fluid>
+        <Nav className='justify-content-center py-3'>
+          <Link href='/fale-conosco'>
+            <Nav.Link href='/fale-conosco'>Associe-se</Nav.Link>
+          </Link>
+          <Link href='/eventos/'>
+            <Nav.Link href='/eventos/'>FaceBook</Nav.Link>
+          </Link>
+          <Link href='/fale-conosco'>
+            <Nav.Link href='/fale-conosco'>Quem Somos</Nav.Link>
+          </Link>
+        </Nav>
+      </Container>
+      <Container className={styles.copyright}>
+        <Nav className={`justify-content-center py-3`}>
+          <Link href='/fale-conosco'>
+            <Nav.Item>Copyrights Brinca - 2020</Nav.Item>
+          </Link>
+        </Nav>
+      </Container>
+    </>
   );
 };
 export default Footer;
