81c80bd8892846ba9463bb482268139463263e30
marceloglacial <glacial@gmail.com>
2020-09-21 22:00:45 -0400

    add styles

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -8,19 +8,21 @@ import { useContext } from 'react';
 const Footer = (props) => {
   const { footerMenu } = useContext(dataContext);
   const menuItems = footerMenu.items;
+  const year = new Date().getUTCFullYear();
+
   return (
     <>
       <Container className={styles.footer} fluid>
         <Nav className='justify-content-center py-4'>
           {menuItems.map((item) => {
-            const { ID, title, slug, icon, url, style } = item;
+            const { ID, title, slug, icon, url } = item;
             if (!slug) {
               return (
                 <Nav.Link
                   key={ID}
                   href={url}
                   target='_blank'
-                  className={`mx-3 ${style && styles.navItemHighlight}`}
+                  className={`mx-3`}
                 >
                   {icon && <i className={`${icon} mr-2`}></i>}
                   {title}
@@ -29,10 +31,7 @@ const Footer = (props) => {
             }
             return (
               <Link href={slug} key={ID}>
-                <Nav.Link
-                  href={slug}
-                  className={`mx-3 ${style && styles.navItemHighlight}`}
-                >
+                <Nav.Link href={slug} className={`mx-3`}>
                   {icon && <i className={`${icon} mr-2`}></i>}
                   {title}
                 </Nav.Link>
@@ -42,8 +41,18 @@ const Footer = (props) => {
         </Nav>
       </Container>
       <Container className={styles.copyright}>
-        <Nav className={`justify-content-center py-3`}>
-          <Nav.Item>Copyrights Brinca - 2020</Nav.Item>
+        <Nav className={`justify-content-center py-3 ${styles.copyrightMenu}`}>
+          <Nav.Item>Copyrights Brinca - {year}</Nav.Item>
+          <Nav.Item>
+            <a href={`http://marceloglacial.com`}>
+              Developed by Marcelo Glacial
+            </a>
+          </Nav.Item>
+          <Nav.Item>
+            <a href={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-admin`}>
+              User Login
+            </a>
+          </Nav.Item>
         </Nav>
       </Container>
     </>
