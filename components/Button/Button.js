5e549dd1781923aa9a3ea5a06ead4f552b61b5a7
marceloglacial <glacial@gmail.com>
2020-11-06 15:35:53 -0500

    update button component

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 
 const Button = (props) => {
-  const { title, link, type = 'primary' } = props;
+  const { title, link, type } = props;
   return (
-    <Link href={link}>
-      <a className={`btn btn-${type}`}>{title}</a>
+    <Link href={link || '#'}>
+      <a className={`btn btn-${type || 'primary'}`}>
+        {title || 'Please add a title'}
+      </a>
     </Link>
   );
 };
