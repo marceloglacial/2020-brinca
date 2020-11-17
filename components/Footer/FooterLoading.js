b90205cae5a51f6e44e056b0439691ee3bb049f8
Marcelo Glacial <marceloglacial@marcelos-mbp.lan>
2020-11-16 19:59:08 -0500

    remove netlify and loading dependencies

--- components/Footer/FooterLoading.js
+++ components/Footer/FooterLoading.js
@@ -1,13 +1,4 @@
-import Skeleton from 'react-loading-skeleton';
-
 const FooterLoading = (props) => {
-  return (
-    <div className='container-fluid mt-4 text-center'>
-      <Skeleton height={70} />
-      <p className='mt-2'>
-        <Skeleton height={15} width={200} />
-      </p>
-    </div>
-  );
+  return <div className='container-fluid mt-4 text-center'>Loading ...</div>;
 };
 export default FooterLoading;
