24ffee9f0161d8fc88910de24a047a796590a762
Marcelo Glacial <marceloglacial@marcelos-mbp.lan>
2020-12-09 19:26:52 -0500

    add last page handle

--- components/Blocks/PostList/PostList.js
+++ components/Blocks/PostList/PostList.js
@@ -1,12 +1,19 @@
 import Button from 'components/Button/Button';
 import PosstListPage from './components/PostListPage';
 import { useEffect, useState } from 'react';
+import usePosts from 'functions/usePosts';
 
 const PostList = (props) => {
   const per_page = 3;
   const [offSet, setOffSet] = useState(0);
   const [pages, setPages] = useState([]);
   const [isLoadingPage, setIsLoadingPage] = useState(false);
+  const { postData, isLoading, isError } = usePosts(
+    null,
+    per_page,
+    offSet + per_page
+  );
+  const hasPosts = !isLoading && !isError && postData.length > 0 ? true : false;
 
   useEffect(() => {
     setPages([<PosstListPage per_page={per_page} key={offSet} />]);
@@ -29,13 +36,15 @@ const PostList = (props) => {
   return (
     <section className={`posts-list`}>
       <div className={`card-grid`}>{pages}</div>
-      <div className={`card-grid__load`}>
-        <Button
-          title={isLoadingPage ? 'Loading ... ' : 'Carregar Mais'}
-          type={isLoadingPage ? 'disable' : 'secondary'}
-          onClick={(e) => handleClick(e)}
-        />
-      </div>
+      {hasPosts && (
+        <div className={`card-grid__load`}>
+          <Button
+            title={isLoadingPage ? 'Loading ... ' : 'Carregar Mais'}
+            type={isLoadingPage ? 'disable' : 'secondary'}
+            onClick={(e) => handleClick(e)}
+          />
+        </div>
+      )}
     </section>
   );
 };
