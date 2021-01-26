c6fefe1d91da05b596c3a58ffd2bf96e69214bbf
Marcelo <glacial@gmail.com>
2021-01-25 21:15:28 -0500

    fix card

--- components/Blocks/InstaFeed/InstaFeed.js
+++ components/Blocks/InstaFeed/InstaFeed.js
@@ -1,6 +1,5 @@
 import useApi from 'hooks/useApi';
 import styles from './InstaFeed.module.scss';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 const InstaFeed = (props) => {
   const { userID, title, itemsNumber = '6', imageType } = props;
@@ -16,49 +15,44 @@ const InstaFeed = (props) => {
   return (
     <section className='instafeed'>
       {title && <h2 className={'pb-5'}>{title}</h2>}
-      <ResponsiveMasonry
-        className={styles.instaFeedGrid}
-        columnsCountBreakPoints={{ 350: 2, 750: 2, 900: 3 }}
-      >
-        <Masonry gutter={'1rem'}>
-          {array.map((item) => {
-            const {
-              id,
-              display_url,
-              thumbnail_src,
-              edge_media_to_caption,
-              shortcode,
-            } = item.node;
-            const title = edge_media_to_caption.edges[0];
-            const imageSrc = isThumbnail ? thumbnail_src : display_url;
+      <div className={styles.instaFeedContainer}>
+        {array.map((item) => {
+          const {
+            id,
+            display_url,
+            thumbnail_src,
+            edge_media_to_caption,
+            shortcode,
+          } = item.node;
+          const title = edge_media_to_caption.edges[0];
+          const imageSrc = isThumbnail ? thumbnail_src : display_url;
 
-            return (
-              <a
-                href={`https://www.instagram.com/p/${shortcode}`}
-                target='_blank'
-                className={styles.card}
-                data-aos='fade-up'
-                key={id}
-              >
-                <img
-                  src={imageSrc}
-                  alt=''
-                  className={isThumbnail ? styles.cardImageThumb : ''}
-                />
-                <div className={styles.cardContainer}>
-                  <div className={styles.cardBody}>
-                    {title && (
-                      <p className={`line-clamp ${styles.cardTitle}`}>
-                        {title.node.text}
-                      </p>
-                    )}
-                  </div>
+          return (
+            <a
+              href={`https://www.instagram.com/p/${shortcode}`}
+              target='_blank'
+              className={styles.card}
+              data-aos='fade-up'
+              key={id}
+            >
+              <img
+                src={imageSrc}
+                alt=''
+                className={isThumbnail ? styles.cardImageThumb : ''}
+              />
+              <div className={styles.cardContainer}>
+                <div className={styles.cardBody}>
+                  {title && (
+                    <p className={`line-clamp ${styles.cardTitle}`}>
+                      {title.node.text}
+                    </p>
+                  )}
                 </div>
-              </a>
-            );
-          })}
-        </Masonry>
-      </ResponsiveMasonry>
+              </div>
+            </a>
+          );
+        })}
+      </div>
     </section>
   );
 };
--- components/Card/Card.js
+++ components/Card/Card.js
@@ -17,44 +17,34 @@ const Card = (props) => {
   } = props;
 
   return (
-    <div className={`card`} key={id} data-aos='fade-up'>
-      {showImage === 'yes' && (
-        <div className={`card-img-top ${!image && `card-img-top--empty`}`}>
-          <CardImage {...image} />
-        </div>
-      )}
-      <div className='card-body'>
-        <div
-          className='card-title'
-          dangerouslySetInnerHTML={{ __html: title }}
-        />
-        {showText === 'yes' && (
-          <div
-            className='card-text'
-            dangerouslySetInnerHTML={{ __html: excerpt }}
-          />
-        )}
-        {showDate === 'yes' && (
-          <p className='card-text'>
-            <small className='text-muted'>{date}</small>
-          </p>
-        )}
+    <div data-aos='fade-up'>
+      <div className={`card`} key={id}>
         <Link href={type + '/' + getSlug(link)}>
-          <a className='btn btn-link' href={type + '/' + getSlug(link)}>
-            Leia mais
-            <svg
-              width='15'
-              height='10'
-              viewBox='0 0 15 10'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-              className={`btn__icon`}
-            >
-              <path
-                d='M0.818581 5.80357L11.6248 5.80357L9.2507 8.13393C8.92324 8.45536 8.92323 8.9375 9.2507 9.25893C9.57816 9.58036 10.0693 9.58036 10.3968 9.25893L14.1626 5.5625C14.4901 5.24107 14.4901 4.75893 14.1626 4.4375L10.3968 0.741071C10.0693 0.419642 9.57816 0.419642 9.2507 0.741071C8.92324 1.0625 8.92324 1.54464 9.2507 1.86607L11.6248 4.19643L0.818581 4.19643C0.409256 4.19643 -7.01282e-05 4.51786 -7.01711e-05 5C-7.02141e-05 5.48214 0.409255 5.80357 0.818581 5.80357Z'
-                fill='#4D4D4D'
+          <a className={'card-link'} href={type + '/' + getSlug(link)}>
+            {showImage === 'yes' && (
+              <div
+                className={`card-img-top ${!image && `card-img-top--empty`}`}
+              >
+                <CardImage {...image} />
+              </div>
+            )}
+            <div className='card-body'>
+              <div
+                className='card-title'
+                dangerouslySetInnerHTML={{ __html: title }}
               />
-            </svg>
+              {showText === 'yes' && (
+                <div
+                  className='card-text'
+                  dangerouslySetInnerHTML={{ __html: excerpt }}
+                />
+              )}
+              {showDate === 'yes' && (
+                <p className='card-text'>
+                  <small className='text-muted'>{date}</small>
+                </p>
+              )}
+            </div>
           </a>
         </Link>
       </div>
