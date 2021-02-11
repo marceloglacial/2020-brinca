0c578e53a76da93026e8c48010d60c7ee3ab9f8d
Marcelo <glacial@gmail.com>
2021-02-10 20:52:31 -0500

    add tag

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <meta
+            name='google-site-verification'
+            content='CIvkus0lq2Y4Cq3fX0SD_ExCeDfeV7EXianwJMGqfh8'
+            key='analytics'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
