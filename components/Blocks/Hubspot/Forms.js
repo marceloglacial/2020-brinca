941fc9f1f3ab41d4a963e3644b9b3984c5092d55
Marcelo <glacial@gmail.com>
2021-05-25 19:02:24 -0400

    add initial form styles

--- components/Blocks/Hubspot/Forms.js
+++ components/Blocks/Hubspot/Forms.js
@@ -2,33 +2,60 @@ import useApi from 'hooks/useApi';
 
 const HsForms = (props) => {
   const { data, isLoading } = useApi('/api/hubspot/forms');
-  console.log(data);
   if (isLoading) return 'loading ...';
+
+  const { name, fieldGroups, legalConsentOptions, displayOptions } = data;
+  const { communicationConsentText, communicationsCheckboxes } =
+    legalConsentOptions;
+
+  console.log(data);
+
   return (
-    <section className='forms'>
-      <header>
-        <h3>{data.name}</h3>
+    <section className='form'>
+      <header className='form__header'>
+        <h3>{name}</h3>
       </header>
       <form className='form'>
-        {data.fieldGroups.map((item, index) => {
+        {fieldGroups.map((item, index) => {
           const { label, required } = item.fields[0];
           return (
-            <div className='forms__group' key={index}>
+            <div className='form__group' key={index}>
               <label className='label'>{label}:</label>
-              <br />
-              <input required={required} />
+              <input className='input' required={required} />
             </div>
           );
         })}
-        <input type='submit' value={data.displayOptions.submitButtonText} />
-      </form>
-      <footer>
-        <h6
-          dangerouslySetInnerHTML={{
-            __html: data.legalConsentOptions.communicationConsentText,
-          }}
+        <div className='form__consent'>
+          <label
+            className='label'
+            dangerouslySetInnerHTML={{
+              __html: communicationConsentText,
+            }}
+          />
+          {communicationsCheckboxes.map((item, index) => {
+            const { label, required } = item;
+            return (
+              <div className='form__group' key={index}>
+                <input
+                  type='checkbox'
+                  className='checkbox'
+                  id={`content__${index}`}
+                  required={required}
+                />
+                <label className='label' htmlFor={`content__${index}`}>
+                  {label}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+
+        <input
+          type='submit'
+          className='btn btn-secondary btn-form'
+          value={displayOptions.submitButtonText}
         />
-      </footer>
+      </form>
     </section>
   );
 };
