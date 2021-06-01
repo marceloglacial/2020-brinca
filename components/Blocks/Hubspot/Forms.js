0b09fd33ddc655638873b6edf59c609d92cf85b3
Marcelo <glacial@gmail.com>
2021-05-31 22:37:37 -0400

    add consent

--- components/Blocks/Hubspot/Forms.js
+++ components/Blocks/Hubspot/Forms.js
@@ -8,6 +8,7 @@ const HsForms = (props) => {
   const { slug = '28b1f0ae-e05a-474e-8366-07be0ddb4ca3', archived } = props;
   const [formData, setFormData] = useState({});
   const [formStatus, setFormStatus] = useState(false);
+  const [consent, setConsent] = useState({});
   const { data, isLoading } = useApi(`/api/hubspot/forms/${slug}`);
 
   if (isLoading) return 'loading ...';
@@ -26,6 +27,13 @@ const HsForms = (props) => {
     }));
   };
 
+  const handleConsent = (e, label) => {
+    setConsent({
+      checked: e.target.checked,
+      label,
+    });
+  };
+
   const formatFormData = (formData) =>
     Object.keys(formData).map(function (key) {
       const value = formData[key];
@@ -33,6 +41,7 @@ const HsForms = (props) => {
     });
 
   // @see https://legacydocs.hubspot.com/docs/methods/forms/submit_form
+  console.log(consent);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -44,6 +53,19 @@ const HsForms = (props) => {
       context: {
         pageUri: 'www.brinca.ca',
         pageName: 'Brinca Website',
+      },
+      legalConsentOptions: {
+        consent: {
+          consentToProcess: true,
+          text: 'I agree to allow Example Company to store and process my personal data.',
+          communications: [
+            {
+              value: consent.checked,
+              subscriptionTypeId: 9584744,
+              text: consent.label,
+            },
+          ],
+        },
       },
     });
 
@@ -59,6 +81,7 @@ const HsForms = (props) => {
       requestOptions
     )
       .then((response) => {
+        response.text();
         response.status === 200 && setFormStatus(true);
       })
       .catch((error) => {
@@ -69,6 +92,7 @@ const HsForms = (props) => {
   const formProps = {
     name,
     handleSubmit,
+    displayOptions,
   };
 
   if (formStatus) {
@@ -85,51 +109,46 @@ const HsForms = (props) => {
   } else {
     return (
       <FormWrapper {...formProps}>
-        <form className='form' onSubmit={(e) => handleSubmit(e)}>
-          {fieldGroups.map((item, index) => (
-            <FormsField
-              {...item.fields[0]}
-              id={`form-id-${index}`}
-              onChange={(e) => handleFieldUpdate(e, item)}
-              key={index}
-            />
-          ))}
-          {communicationsCheckboxes && (
-            <div className='form-consent'>
-              <label
-                className='label'
-                dangerouslySetInnerHTML={{
-                  __html: communicationConsentText,
-                }}
-              />
-              {communicationsCheckboxes.map((item, index) => {
-                const { label, required } = item;
-                return (
-                  <div
-                    className='form__group form__group--checkbox'
-                    key={index}
-                  >
-                    <input
-                      type='checkbox'
-                      className='checkbox'
-                      id={`content__${index}`}
-                      required={required}
-                    />
-                    <label className='label' htmlFor={`content__${index}`}>
-                      {label}
-                    </label>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-
-          <input
-            type='submit'
-            className='btn btn-secondary btn-form'
-            value={displayOptions.submitButtonText}
+        {fieldGroups.map((item, index) => (
+          <FormsField
+            {...item.fields[0]}
+            id={`form-id-${index}`}
+            onChange={(e) => handleFieldUpdate(e, item)}
+            key={index}
           />
-        </form>
+        ))}
+        {communicationsCheckboxes && (
+          <div className='form-consent'>
+            <label
+              className='label'
+              dangerouslySetInnerHTML={{
+                __html: communicationConsentText,
+              }}
+            />
+            {communicationsCheckboxes.map((item, index) => {
+              const { label, required } = item;
+              return (
+                <div className='form__group form__group--checkbox' key={index}>
+                  <input
+                    type='checkbox'
+                    className='checkbox'
+                    id={`content__${index}`}
+                    required={required}
+                    onChange={(e) => handleConsent(e, label)}
+                  />
+                  <label className='label' htmlFor={`content__${index}`}>
+                    {label}
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+        )}
+        <input
+          type='submit'
+          className='btn btn-secondary btn-form'
+          value={displayOptions.submitButtonText}
+        />
       </FormWrapper>
     );
   }
