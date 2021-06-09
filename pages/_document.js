eb2c8ece6b541242e55a956bd125a75579da6474
Marcelo <glacial@gmail.com>
2021-06-08 20:33:40 -0400

    add preload

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,19 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Mulish:wght@200;400;700&display=swap'
+            rel='stylesheet'
+          />
+
           <meta
             name='google-site-verification'
             content='CIvkus0lq2Y4Cq3fX0SD_ExCeDfeV7EXianwJMGqfh8'
             key='analytics'
           />
+
+          <link rel='preconnect' href='https://forms.hubspot.com' />
           <script
             type='text/javascript'
             id='hs-script-loader'
