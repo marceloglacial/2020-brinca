a8ac027eb1a3914764a32f785e1a2545156b19d7
Marcelo <glacial@gmail.com>
2021-07-09 11:13:22 -0400

    add google analytics and remove hubspot scripts

--- pages/_document.js
+++ pages/_document.js
@@ -15,20 +15,27 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Mulish:wght@200;400;700&display=swap'
             rel='stylesheet'
           />
-
           <meta
             name='google-site-verification'
             content='CIvkus0lq2Y4Cq3fX0SD_ExCeDfeV7EXianwJMGqfh8'
             key='analytics'
           />
-
-          <link rel='preconnect' href='https://forms.hubspot.com' />
+          <link rel='preconnect' href='https://www.googletagmanager.com' />
           <script
-            type='text/javascript'
-            id='hs-script-loader'
             async
-            defer
-            src='//js.hs-scripts.com/7882714.js'
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
