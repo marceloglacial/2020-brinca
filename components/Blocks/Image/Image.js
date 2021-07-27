8f1b6108f35d64bff7efede79d9963ee42cecd7e
Marcelo <glacial@gmail.com>
2021-07-27 11:56:36 -0400

    image defaults

--- components/Blocks/Image/Image.js
+++ components/Blocks/Image/Image.js
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import styles from './Image.module.scss';
 
 const ImageCore = (props) => {
-  const { src, alt, caption, width, height } = props.attrs;
+  const { media, imageSize = 'full' } = props.attrs || props.attributes;
+  const { url, width, height } = media?.sizes[imageSize];
+  const { alt, caption } = media;
   return (
     <figure className={`${styles.figure} mt-4 mb-5`} data-aos='fade-in'>
       <Image
-        src={src}
+        src={url}
         alt={alt}
         width={width || 800}
         height={height || 600}
