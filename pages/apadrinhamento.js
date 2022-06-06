f4d6f7d2a31f5ae061672de1c61ef9fc962e4c57
dutraneto <dutraneto@me.com>
2022-06-06 16:33:50 -0400

    Add getStaticProps to pull up nav and footer

--- pages/apadrinhamento.js
+++ pages/apadrinhamento.js
@@ -1,17 +1,41 @@
 import Layout from 'components/Layout/Layout'
 import PadrinhoContent from 'components/Padrinho/PadrinhoContent'
 import Button from 'components/Button/Button'
+import { getData } from 'functions/getData';
 
 const Apadrinhamento = (props) => {
+  const { headerMenu } = props
+
   return (
-    <Layout pageTitle={`Torne-se um Padrinho`}>
+    <Layout pageTitle={`Torne-se um Padrinho`} {...props}>
       <PadrinhoContent title={`Apadrinhe um recém-chegado ao Canada!`} />
-      <div className={`text-center`}>
+      <section className={`text-center`}>
         <Button title={`Quero apadrinhar um recém chegado`} link={`/apadrinhar`} type={`primary`} className={`mb-3 mr-2`} />
         <Button title={`Quero ser apadrinhado`} link={`/ser-apadrinhado`} type={`primary`} className={`mb-3`} />
-      </div>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allData = (await getData()) || {};
+  const {
+    frontPage = [],
+    headerMenu = [],
+    footerMenu = [],
+    subscribeMenu = [],
+    socialMenu = [],
+  } = allData;
+  return {
+    props: {
+      frontPage,
+      headerMenu,
+      footerMenu,
+      subscribeMenu,
+      socialMenu,
+    },
+    revalidate: 30,
+  };
 }
 
 export default Apadrinhamento
