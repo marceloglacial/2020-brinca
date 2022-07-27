8c903719bd3b1595654961eeb742e29f44884427
dutraneto <dutraneto@me.com>
2022-07-27 08:34:33 -0400

    Show menu on mobile

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -9,17 +9,17 @@ const Header = (props) => {
       <div className={`navbar-brand mx-0 ${styles.brandLogo}`}>
         <Logo />
       </div>
-      <nav className={`navbar navbar-expand-lg`}>
-        <div className={`d-flex ${styles.navigation}`}>
-          <input type="checkbox" className={`${styles.navigation__checkbox}`} id="nav__toggle" />
-          <label htmlFor="nav__toggle" className={`${styles.navigation__button}`}>
-            <span className={`${styles.navigation__icon}`}>&nbsp;</span>
-          </label>
-          <div className={`${styles.navigation__background}`}>&nbsp;</div>
+      <div className={`d-flex ${styles.navigation}`}>
+        <input type="checkbox" className={`${styles.navigation__checkbox}`} id="nav__toggle" />
+        <label htmlFor="nav__toggle" className={`${styles.navigation__button}`}>
+          <span className={`${styles.navigation__icon}`}>&nbsp;</span>
+        </label>
+        <div className={`${styles.navigation__background}`}>&nbsp;</div>
+        <nav className={`navbar navbar-expand-lg ${styles.navigation__nav}`}>
           <HeaderNav {...props} />
           <MenuSubscribe {...props} />
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };
