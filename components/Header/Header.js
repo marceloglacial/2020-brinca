54a4cae0ec402978577724126800bc2816580da9
dutraneto <dutraneto@me.com>
2022-07-27 07:36:56 -0400

    Create menu hamburguer

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,18 +4,46 @@ import HeaderNav from './HeaderNav';
 import styles from './Header.module.scss';
 
 const Header = (props) => {
+  console.log(styles)
   return (
-    <nav className={`navbar navbar-expand-lg ${styles.navbarContainer}`}>
-      <div className='container'>
-        <div className={`navbar-brand mx-0 ${styles.brandLogo}`}>
-          <Logo />
-        </div>
-        <div className='collapse navbar-collapse'>
+    <div className={`container d-flex justify-content-between ${styles.navbarContainer}`}>
+      <div className={`navbar-brand mx-0 ${styles.brandLogo}`}>
+        <Logo />
+      </div>
+      <nav className={`navbar navbar-expand-lg`}>
+        <div className={`d-flex ${styles.navigation}`}>
+          <input type="checkbox" className={`${styles.navigation__checkbox}`} id="nav__toggle" />
+          <label htmlFor="nav__toggle" className={`${styles.navigation__button}`}>
+            <span className={`${styles.navigation__icon}`}>&nbsp;</span>
+          </label>
+          <div className={`${styles.navigation__background}`}>&nbsp;</div>
           <HeaderNav {...props} />
           <MenuSubscribe {...props} />
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 export default Header;
+
+
+{/* <div class="navigation">
+  <input type="checkbox" class="navigation__checkbox" id="nav__toggle" />
+
+  <label for="nav__toggle" class="navigation__button">
+      // navigation line
+    <span class="navigation__icon">&nbsp;</span>
+  </label>
+
+  <div class="navigation__background">&nbsp;</div>
+
+  <nav class="navigation__nav">
+    <ul class="navigation__list">
+      <li class="navigation__item"><a href="#" class="navigation__link"><span>01</span>About Natours</a></li>
+      <li class="navigation__item"><a href="#" class="navigation__link"><span>02</span>Your Benefits</a></li>
+      <li class="navigation__item"><a href="#" class="navigation__link"><span>03</span>Popular Tours</a></li>
+      <li class="navigation__item"><a href="#" class="navigation__link"><span>04</span>Stories</a></li>
+      <li class="navigation__item"><a href="#" class="navigation__link"><span>05</span>Book Now</a></li>
+    </ul>
+  </nav>
+</div> */}
