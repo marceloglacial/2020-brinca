6c1cbae095fe5f8e9cccb6e46bff472ba014aa5e
dutraneto <dutraneto@me.com>
2022-07-27 07:39:41 -0400

    Remove comment lines, remove logs

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,7 +4,6 @@ import HeaderNav from './HeaderNav';
 import styles from './Header.module.scss';
 
 const Header = (props) => {
-  console.log(styles)
   return (
     <div className={`container d-flex justify-content-between ${styles.navbarContainer}`}>
       <div className={`navbar-brand mx-0 ${styles.brandLogo}`}>
@@ -25,25 +24,3 @@ const Header = (props) => {
   );
 };
 export default Header;
-
-
-{/* <div class="navigation">
-  <input type="checkbox" class="navigation__checkbox" id="nav__toggle" />
-
-  <label for="nav__toggle" class="navigation__button">
-      // navigation line
-    <span class="navigation__icon">&nbsp;</span>
-  </label>
-
-  <div class="navigation__background">&nbsp;</div>
-
-  <nav class="navigation__nav">
-    <ul class="navigation__list">
-      <li class="navigation__item"><a href="#" class="navigation__link"><span>01</span>About Natours</a></li>
-      <li class="navigation__item"><a href="#" class="navigation__link"><span>02</span>Your Benefits</a></li>
-      <li class="navigation__item"><a href="#" class="navigation__link"><span>03</span>Popular Tours</a></li>
-      <li class="navigation__item"><a href="#" class="navigation__link"><span>04</span>Stories</a></li>
-      <li class="navigation__item"><a href="#" class="navigation__link"><span>05</span>Book Now</a></li>
-    </ul>
-  </nav>
-</div> */}
