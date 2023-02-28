409da9f2e2e30424bd55c8e1fde2afce841eadc6
Marcelo <glacial@gmail.com>
2023-02-28 17:41:50 -0500

    update events sor

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 export const API_PAGES_PARAMS = `pages?populate=content,thumbnail`;
-export const API_EVENTS_PARAMS = `events?populate=content,thumbnail`;
+export const API_EVENTS_PARAMS = `events?populate=content,thumbnail&sort=date:desc`;
 export const API_MENU = `menu?populate=*`;
 export const API_BLOCKS = `populate=content.photos,content.buttons,content.image,thumbnail`;
